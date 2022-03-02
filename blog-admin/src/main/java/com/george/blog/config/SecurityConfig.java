53f61fd4603454df61e342564dd772d27b6b83bc
linzhuangze <linzhuangze@do1.com.cn>
2022-03-02 12:13:52 +0800

    feat：springsecurity

--- blog-admin/src/main/java/com/george/blog/config/SecurityConfig.java
+++ blog-admin/src/main/java/com/george/blog/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.george.blog.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 
 /**
  * @description 权限管理配置
  * @author georgeLin
  * @date 2022-02-27-22:33
  */
+@Configuration
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     /**
      * 密码加密器
@@ -23,5 +28,47 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         super.configure(web);
+    }
+
+    @Bean
+    public HttpFirewall httpFirewall() {
+        return new DefaultHttpFirewall();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests() //开启登录认证
+                .antMatchers("/css/**").permitAll()
+                .antMatchers("/img/**").permitAll()
+                .antMatchers("/js/**").permitAll()
+                .antMatchers("/plugins/**").permitAll()
+                //SPEL表达式@Bean：从容器中查询Bean
+                .antMatchers("/admin/**").access("@authService.auth(request,authentication)") //自定义service 来去实现实时的权限认证
+                .antMatchers("/pages/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                //表单登录
+                .formLogin()
+                .loginPage("/login.html") //自定义的登录页面
+                .loginProcessingUrl("/login") //登录处理接口
+                .usernameParameter("username") //定义登录时的用户名的key 默认为username
+                .passwordParameter("password") //定义登录时的密码key，默认是password
+                .defaultSuccessUrl("/pages/main.html")
+                .failureUrl("/login.html")
+                .permitAll() //通过 不拦截，更加前面配的路径决定，这是指和登录表单相关的接口 都通过
+                .and()
+                //登出
+                .logout() //退出登录配置
+                .logoutUrl("/logout") //退出登录接口
+                .logoutSuccessUrl("/login.html")
+                .permitAll() //退出登录的接口放行
+                .and()
+                //http基本认证
+//                .httpBasic()
+//                .and()
+                //
+                .csrf().disable() //csrf关闭 如果自定义登录 需要关闭
+                .headers().frameOptions().sameOrigin();//支持frame,设置X-Frame-Options: SAMEORIGIN
     }
 }
